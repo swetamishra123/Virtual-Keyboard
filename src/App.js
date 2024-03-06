2f906267ad6af8693593a51e1378c8b9006f474a
swetamishra123 <swetamishra603@gmail.com>
2024-03-06 19:34:48 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import Keyboard from './components/Keyboard'; 
+  
+function App() { 
+  return ( 
+    <div className="App"> 
+      <Keyboard/> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
